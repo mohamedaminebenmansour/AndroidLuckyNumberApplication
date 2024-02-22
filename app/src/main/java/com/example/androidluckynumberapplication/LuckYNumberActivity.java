af07bf2ba97450f16d209f669718a4dee0923bba
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-22 23:30:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androidluckynumberapplication/LuckYNumberActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidluckynumberapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LuckYNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luck_ynumber);
+    }
+}
